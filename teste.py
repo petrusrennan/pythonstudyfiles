f93bf0fa3019f9e535e69d232b89e6fdfdf9761e
petrusrennan <36818079+petrusrennan@users.noreply.github.com>
2018-03-03 22:26:11 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -12,16 +12,29 @@ print("")
 #FIM DA INTERFACE GRÁFICA#
 
 #TAGS DA LINGUAGEM DE PROGRAMAÇÃO HARMONY#
-keylogger="keylogger"
-redef="redef"
+keylogger="badscan"
+redef="redefinir"
 helpme="helpme"
+lista="hist_entry"
+tent="tent"
 #FIM DAS TAGS#
 
 #SISTEMA#
-senha="pattern"
+senha="HISTÓRICO DE ENTRADA DE DADOS"
+rangex=0
 password="tacos"
+listagem=[senha]
+tentativas=0
+senha=input('Digite a senha: ')
 while(senha!=password):
+        rangex=rangex+1
+        listagem+=[senha]
         if(senha==redef):
+                #CONSOLE DE TENTATIVAS# 
+                tentativas=tentativas-1
+                if(tentativas<=0):
+                        tentativas+=1
+                #FIM DO CONSOLE#
                 oldpass=input("Digite a antiga senha:")                
                 if(oldpass==password):
                         rasc=input("Digite a nova senha: ")
@@ -35,22 +48,55 @@ while(senha!=password):
                         print("Essa não é a antiga senha!")
                         
         if(senha==keylogger):
+                #CONSOLE DE TENTATIVAS# 
+                tentativas=tentativas-1
+                if(tentativas<=0):
+                        tentativas+=1
+                #FIM DO CONSOLE#
                 print("Analisando senhas impróprias...")
                 if(password=="babaca"):
+                        print("SENHA IMPRÓPRIA DETECTADA! SENHA PADRÃO REDEFINIDA!")
+                        password="tacos"
+                elif(password=="trouxa"):
+                        print("SENHA IMPRÓPRIA DETECTADA! SENHA PADRÃO REDEFINIDA!")
+                        password="tacos"
+                elif(password=="escroto"):
+                        print("SENHA IMPRÓPRIA DETECTADA! SENHA PADRÃO REDEFINIDA!")
+                        password="tacos"
+                elif(password=="PCC"):
+                        print("SENHA IMPRÓPRIA DETECTADA! SENHA PADRÃO REDEFINIDA!")
+                        password="tacos"
+                elif(password=="CV"):
+                        print("SENHA IMPRÓPRIA DETECTADA! SENHA PADRÃO REDEFINIDA!")
+                        password="tacos"
+                elif(password=="GDR"):
+                        print("SENHA IMPRÓPRIA DETECTADA! SENHA PADRÃO REDEFINIDA!")
+                        password="tacos"
+                elif(password=="bandido"):
+                        print("SENHA IMPRÓPRIA DETECTADA! SENHA PADRÃO REDEFINIDA!")
+                        password="tacos"
+                elif(password=="criminoso"):
                         print("SENHA IMPRÓPRIA DETECTADA! SENHA PADRÃO REDEFINIDA!")
                         password="tacos"
                 else:
                         print("Nenhuma senha imprópria foi detectada!")
         if(senha==helpme):
+                #CONSOLE DE TENTATIVAS# 
+                tentativas=tentativas-1
+                if(tentativas<=0):
+                        tentativas+=1
+                #FIM DO CONSOLE# 
                 print("")
 
                 print("-----------------------------------------")
                 print("ENCICLOPÉDIA DE TAGS DA LINGUAGEM HARMONY")
                 print("-----------------------------------------")
                 print("")
-                print("redef: redefine uma senha.")
-                print("keylogger: escaneia palavras proibidas nas senhas.")
+                print("redefinir: redefine uma senha.")
+                print("badscan: escaneia palavras proibidas nas senhas.")
                 print("helpme: Abre a enciclopédia da linguagem HARMONY.")
+                print("tent: mostra o número de tentativas de introdução da senha.")
+                print("hist_entry: Exibe o histórico de entrada de dados.")
                 print("")
                 repeat=input("repetir?(s/n): ")
                 while(repeat!="n"):
@@ -61,20 +107,47 @@ while(senha!=password):
                         print("")
                         repeat=input("repetir?(s/n): ")
                 byebye=input("ATÉ LOGO! (PRESSIONE QUALQUER TECLA PRA SAIR...)")
+        if(senha==tent):
+
+               #CONSOLE DE TENTATIVAS# 
+                tentativas=tentativas-1
+                if(tentativas<=0):
+                        tentativas+=1
+               #FIM DO CONSOLE# 
+                print("Número de tentativas:",tentativas)
+                
+        if(senha==lista):
+                #CONSOLE DE TENTATIVAS# 
+                tentativas=tentativas-1
+                if(tentativas<=0):
+                        tentativas+=1       
+                for trials in range(rangex):
+                        print(listagem[trials])
+
+                        
+        #DIRETRIZ DE SEGURANÇA#
+        if(tentativas>=3):
+                error=input("DIRETRIZ DE SEGURANÇA ACIONADA! DESLIGANDO SISTEMAS!")
+                break
+        #FIM DA DIRETRIZ#
+
+                
+                
+                
 
                 
                 
                 
         print("")
         senha=input('Digite a senha: ')
+        tentativas=tentativas+1
         
         
                 
         
-        
-        
 if senha == password:
 	print('Acesso Liberado!')
+	print('A senha para os 5 milhões de dólares é: HTXTORF589')
+	
 	
 #FIM DO SISTEMA#
-
